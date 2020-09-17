d05ea9db03a03d00f785fd59af2c9138293ee146
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2020-09-17 15:11:58 +0200

    adding home.php

--- src/ressources/views/layout.php
+++ src/ressources/views/layout.php
@@ -4,15 +4,15 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="../css/main.css" rel="stylesheet">
-    <title><!-- TITLE --></title>
+    <title>ExerciseLooper</title>
 </head>
 <body>
     <header class="home-header">
         <img src="../images/logo.png">
-        <div>
-            <h1>E x e r c i s e</h1>
-            <h1>L o o p e r</h1>
-        </div>
+        <?= $header; ?>
     </header>
+
+    <?= $content; ?>
+
 </body>
 </html>
