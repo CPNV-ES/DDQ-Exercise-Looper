e0d6d1e9b32759e9f3009fe82149387dfb7d3202
Dimitri Imfeld <dimfeld@protonmail.ch>
2020-09-24 15:41:59 +0200

    Add first version of the Router

---
+++ src/app/Router.php
@@ -0,0 +1,61 @@
+<?php
+namespace Looper\App;
+
+class Router {
+    private $routes;
+
+    public function __construct() {
+        $this->routes = [];
+    }
+
+    public function run($requestUri = null, $requestMethod = null) {
+        $method = isset($requestMethod) ? $requestMethod : $_SERVER["REQUEST_METHOD"];
+        $requestUri = isset($requestUri) ? $requestUri : $_SERVER["REQUEST_URI"];
+        $requestUri = rtrim($requestUri, "/");
+
+        foreach($this->routes as $route) {
+            $pattern = "@^" . preg_replace('/\\\:[a-zA-Z0-9\_\-]+/', '([a-zA-Z0-9\-\_]+)', preg_quote($route['url'])) . "$@D";
+            $matches = [];
+            if(in_array($method, $route["methods"]) && preg_match($pattern, $requestUri, $matches)) {
+                array_shift($matches);  // remove first match as it contains the whole request uri
+                var_dump($matches);
+
+                $actionType = gettype($route["action"]);
+                if($actionType == "string" && strpos($route["action"], "::") != -1 ) {
+                    $parts = explode("::", $route["action"]);
+                    $controllerName = $parts[0];
+                    $actionName = $parts[1];
+                    echo "Controller: " . $controllerName . " -- Action: " .$actionName;
+                    include "src/app/Controllers/" . $controllerName . ".php";
+
+                    $controller = new $controllerName;
+                    call_user_func_array([$controller, $actionName], $matches);
+                }
+                elseif($actionType == "object" && is_callable($route["action"])) {
+                    call_user_func_array($route["action"], $matches);
+                }
+
+                return;
+            }
+        }
+
+        $this->error_404();
+    }
+
+    public function add($url, $action, $methods = "GET") {
+        if(gettype($methods) == "string")
+            $methods = [$methods];
+
+        $url = rtrim($url, "/");
+
+        array_push($this->routes, [
+            "url" => $url,
+            "action" => $action,
+            "methods" => $methods,
+        ]);
+    }
+
+    public function error_404() {
+        echo "404 lole";
+    }
+}
