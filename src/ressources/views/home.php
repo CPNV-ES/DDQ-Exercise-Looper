ca6fdbe8eed840769a5c15dc746a4220018e9409
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2020-09-24 15:40:55 +0200

    Adding the take, create, manage exercise pages

--- src/ressources/views/home.php
+++ src/ressources/views/home.php
@@ -8,13 +8,13 @@ $header =
 $content =
     '<div class="row">
         <div class="column">
-            <a href="" class="button primary-color text-white">TAKE AN EXERCISE</a>
+            <a href="take_exercise.php" class="button primary-color text-white">TAKE AN EXERCISE</a>
         </div>
         <div class="column">
-            <a href="" class="button create-color text-white">CREATE AN EXERCISE</a>
+            <a href="create_exercise.php" class="button create-color text-white">CREATE AN EXERCISE</a>
         </div>
         <div class="column">
-            <a href="" class="button manage-color text-white">MANAGE AN EXERCISE</a>
+            <a href="manage_exercise.php" class="button manage-color text-white">MANAGE AN EXERCISE</a>
         </div>
      </div>';
 
