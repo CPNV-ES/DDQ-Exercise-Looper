70d4693ce1a3c36106c35a29baa86b9e9dd2daf0
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2020-10-01 14:55:03 +0200

    Add the routes for the exercises controller

--- src/public/index.php
+++ src/public/index.php
@@ -4,4 +4,7 @@ require_once "../app/Router.php";
 $webrouter = new \Looper\App\Router();
     
 $webrouter->add("/exercises", "ExerciseController::index");
+$webrouter->add("/exercises/new", "ExerciseController::new");
+$webrouter->add("/exercises/take", "ExerciseController::take");
+
 $webrouter->run();
