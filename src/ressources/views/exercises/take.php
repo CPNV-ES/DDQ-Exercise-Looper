030733e21a22d08e90f1987e838ebdc848a508b8
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2020-10-06 15:04:46 +0200

    Add take page header

--- src/ressources/views/exercises/take.php
+++ src/ressources/views/exercises/take.php
@@ -6,15 +6,10 @@ $header = '';
 
 $content =
     '<div class="row">
-        <div class="column">
-            <a href="/exercises/take" class="button primary-color text-white">TAKE AN EXERCISE</a>
+        <div class="col-1">
+                    
         </div>
-        <div class="column">
-            <a href="/exercises/new" class="button create-color text-white">CREATE AN EXERCISE</a>
-        </div>
-        <div class="column">
-            <a href="/exercises" class="button manage-color text-white">MANAGE AN EXERCISE</a>
-        </div>
-     </div>';
+    </div>
+    ';
 
-require_once "layout.php";
+require_once "../ressources/views/layout.php";
