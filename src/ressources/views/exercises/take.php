fd034f2681725642ff28e2962537b0ce4d8938df
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2020-10-06 11:50:53 +0200

    Add responsive classes for the columns

--- src/ressources/views/exercises/take.php
+++ src/ressources/views/exercises/take.php
@@ -1,1 +1,20 @@
-Take an exercise
+<?php
+
+$headerClass = 'basic-header primary-color';
+
+$header = '';
+
+$content =
+    '<div class="row">
+        <div class="column">
+            <a href="/exercises/take" class="button primary-color text-white">TAKE AN EXERCISE</a>
+        </div>
+        <div class="column">
+            <a href="/exercises/new" class="button create-color text-white">CREATE AN EXERCISE</a>
+        </div>
+        <div class="column">
+            <a href="/exercises" class="button manage-color text-white">MANAGE AN EXERCISE</a>
+        </div>
+     </div>';
+
+require_once "layout.php";
