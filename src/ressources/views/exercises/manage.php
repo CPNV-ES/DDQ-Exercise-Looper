3aa7ac4f7a49d0c34ded185d1e78fbf744432e3c
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2020-10-27 14:46:18 +0100

    test

--- src/ressources/views/exercises/manage.php
+++ src/ressources/views/exercises/manage.php
@@ -1,1 +1,20 @@
-Manage exercise
+<?php
+
+$headerClass = 'header-basic bg-manage';
+
+$header = '<span class="text-white">New exercise</span>';
+
+$content = '
+    <div class="row">
+        <div class="col-3">
+        <span class="page-title">Building</span>
+        </div>
+        <div class="col-3">
+            <span class="page-title">Answering</span>
+        </div>
+        <div class="col-3">
+            <span class="page-title">Closed</span>
+        </div>
+    </div>';
+
+require_once "../ressources/views/layout.php";
