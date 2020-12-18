8ccdec92c0d46f7e647885eca00bd08d5993bee1
SuperQuentin <quentin.aellen@cpnv.ch>
2020-12-18 18:09:10 +0100

    add the store method

--- src/app/Controllers/QuestionController.php
+++ src/app/Controllers/QuestionController.php
@@ -11,4 +11,9 @@ class QuestionController {
     require_once "../ressources/views/questions/index.php";
   }
 
+  function store($id){
+    $questionFieldsModel = new QuestionField();
+    $questionFieldsModel->insert([$_POST['label'],$id,$_POST['value']]);
+    header('Location:/exercises/'.$id.'/questions-fields');
+  }
 }
