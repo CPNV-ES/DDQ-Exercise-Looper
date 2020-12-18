45ede8aa2b99f10954f10d303a9ccb02578b92a0
SuperQuentin <quentin.aellen@cpnv.ch>
2020-12-18 19:02:34 +0100

    update option to select the right one by default

--- src/ressources/views/questions/edit.php
+++ src/ressources/views/questions/edit.php
@@ -11,10 +11,10 @@ $content = '
             <input id="label" class="input-text" name="label" type="text" value="'.$questionField[0]['label'].'" required/>
 
             <label class="input-title" for="valuekind">Value kind</label>
-            <select class="input-text" id="valuekind" name="value" selected="'.$questionField[0]['valueType'].'">
-                <option value="Single line text">Single line text</option>
-                <option value="Multi-line text">Multi-line text</option>
-                <option value="List of single lines">List of single lines</option>
+            <select class="input-text" id="valuekind" name="value">
+                <option value="Single line text" '.($questionField[0]['valueType']=='Single line text'?'selected':'').'>Single line text</option>
+                <option value="Multi-line text" '.($questionField[0]['valueType']=='Multi-line text'?'selected':'').'>Multi-line text</option>
+                <option value="List of single lines" '.($questionField[0]['valueType']=='List of single lines'?'selected':'').'>List of single lines</option>
             </select>
 
             <input class="button bg-primary text-white" type="submit" name="commit" value="Create Question field">
